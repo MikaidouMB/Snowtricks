d329740af6ec80823a3d65cbca6ca6ed1cf695d7
mikaidou <mikaidoumbo@hotmail.fr>
2022-05-20 20:21:54 +0200

    refact mess controller

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -19,66 +19,6 @@ use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 class MessageController extends AbstractController
 {
     /**
-     * @Route("/", name="message_index", methods={"GET"})
-     */
-    public function index(MessageRepository $messageRepository): Response
-    {
-        return $this->render('message/index.html.twig', [
-            'messages' => $messageRepository->findAll(),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="message_new", methods={"GET", "POST"})
-     */
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $message = new Message();
-        $form = $this->createForm(MessageType::class, $message);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($message);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('message_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('message/new_message.html.twig', [
-            'message' => $message,
-            'form' => $form,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="message_show", methods={"GET"})
-     */
-    public function show(Message $message): Response
-    {
-        return $this->render('message/show.html.twig', [
-            'message' => $message,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="message_edit", methods={"GET", "POST"})
-     */
-    public function edit(Request $request, Message $message, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(MessageType::class, $message);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-            return $this->redirectToRoute('message_index', [], Response::HTTP_SEE_OTHER);
-        }
-        return $this->renderForm('message/edit.html.twig', [
-            'message' => $message,
-            'form' => $form,
-        ]);
-    }
-
-    /**
      * @Route("/delete/{id}/", name="message_delete", methods={"DELETE", "GET", "POST"})
      * @param \Symfony\Component\HttpFoundation\Request $request
      * @param \App\Entity\Message $message
